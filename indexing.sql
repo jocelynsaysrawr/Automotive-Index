00c736689503c4ee90e88d13ee583a498c78c8b9
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-03-31 14:45:30 -1000

    indexing

--- indexing.sql
+++ indexing.sql
@@ -17,17 +17,21 @@ CREATE DATABASE indexed_cars;
 
 --\timing to toggle on timing
 
-SELECT DISTINCT ON (make_title) make_title FROM car_models WHERE make_code = 'LAM'; 
---25.091ms
+SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM'; --25.091ms
+SELECT DISTINCT model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R'; --42.229ms
+SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM'; --29.730ms
+SELECT * FROM car_models WHERE year BETWEEN 2010 and 2015; --69.780ms
+SELECT * FROM car_models WHERE year = 2010; --36.254ms
 
-SELECT DISTINCT ON (model_title) model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
---42.229ms
 
-SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
---29.730ms
+CREATE INDEX make_code_index ON car_models (make_code);
+CREATE INDEX year_index ON car_models (year);
+CREATE INDEX model_code_index ON car_models (model_code);
+CREATE INDEX make_and_model_code_index ON car_models (make_code, model_code);
 
-SELECT * FROM car_models WHERE year BETWEEN 2010 and 2015;
---69.780ms
 
-SELECT * FROM car_models WHERE year = 2010;
---36.254ms
+SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM'; --7.511ms
+SELECT DISTINCT model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R'; --2.339ms
+SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM'; --3.936ms
+SELECT * FROM car_models WHERE year BETWEEN 2010 and 2015; -- 91.347ms
+SELECT * FROM car_models WHERE year = 2010; --19.961ms
