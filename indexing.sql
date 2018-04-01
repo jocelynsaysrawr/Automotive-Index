cda624647b5d42dfd8aa4cb9ec6bd0df7cf4c92c
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-03-31 14:20:31 -1000

    timing select statements

--- indexing.sql
+++ indexing.sql
@@ -14,3 +14,20 @@ CREATE DATABASE indexed_cars;
 \i scripts/car_model_data.sql;
 \i scripts/car_model_data.sql;
 \i scripts/car_model_data.sql;
+
+--\timing to toggle on timing
+
+SELECT DISTINCT ON (make_title) make_title FROM car_models WHERE make_code = 'LAM'; 
+--25.091ms
+
+SELECT DISTINCT ON (model_title) model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
+--42.229ms
+
+SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
+--29.730ms
+
+SELECT * FROM car_models WHERE year BETWEEN 2010 and 2015;
+--69.780ms
+
+SELECT * FROM car_models WHERE year = 2010;
+--36.254ms
